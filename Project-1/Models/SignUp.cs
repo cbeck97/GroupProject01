560b1e09d6350c8211549164e2f954541448edaa
Carter Beck <46453763+cbeck97@users.noreply.github.com>
2021-03-16 12:16:08 -0600

    Initial check-in of module Project-1

---
+++ Project-1/Models/SignUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Project_1.Models
+{
+    public class SignUp
+    {
+        public SignUp()
+        {
+        }
+        [Key]
+        
+        public int SubmissionID {get; set;}
+        public string GroupName {get; set;}
+        public int GroupSize {get; set;}
+        public string EmailAddress {get; set;}
+        //[RegularExpression("^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$)"]
+        public string PhoneNumber {get; set;}
+        [ForeignKey("Appointments")]
+        public int? AppointmentID {get; set;}
+
+
+    }
+}
