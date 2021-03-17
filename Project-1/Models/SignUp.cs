b564232dfccc3431fd9c0849d61a9e0400e9c5cb
Dallin Jared <dallin.jared14@gmail.com>
2021-03-17 13:37:46 -0600

    ViewAppointmentViewModel

--- Project-1/Models/SignUp.cs
+++ Project-1/Models/SignUp.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace Project_1.Models
@@ -11,8 +12,12 @@ namespace Project_1.Models
         [Key]
         
         public int SubmissionID {get; set;}
+        [Required]
         public string GroupName {get; set;}
+        [Required]
+        [DefaultValue(1)]
         public int GroupSize {get; set;}
+        [Required]
         public string EmailAddress {get; set;}
         //[RegularExpression("^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$)"]
         public string PhoneNumber {get; set;}
---
+++ Project-1/Models/ViewModels/ViewAppointmentsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Project_1.Models.ViewModels
+{
+    public class ViewAppointmentsViewModel
+    {
+        public ViewAppointmentsViewModel()
+        {
+        }
+    }
+}
